ebd4ed77a42b4353cd6d625359f4ede0927594c3
Cyn-sharp <cyndrickabejo@gmail.com>
2026-09-07 22:35:00 +0800

    Build How-It-Works Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { HeroSection } from "@/components/hero/HeroSection";
 import { TrustSection } from "@/components/trust/TrustSection";
+import { HowItWorks } from "@/components/how-it-works/HowItWorks";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <TrustSection />
+      <HowItWorks />
     </>
   );
 }
